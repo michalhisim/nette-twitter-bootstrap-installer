9b7d68df2e6e715b88a8383d3dfa552e35ed7c71
Michal Šimon <michal.simon@centrum.cz>
2013-04-29 23:41:48 +0200

    ScriptHandler moved to src dir

--- src/ScriptHandler.php
+++ src/ScriptHandler.php
@@ -4,20 +4,17 @@ namespace michalhisim\NetteTwitterBootstrapInstaller\Composer;
 
 use \Composer\Script\CommandEvent;
 
-class ScriptHandler
-{
-    static public function postInstall(CommandEvent $event)
-    {
+class ScriptHandler {
+
+    static public function postInstall(CommandEvent $event) {
         self::installBootstrap($event);
     }
 
-    static public function postUpdate(CommandEvent $event)
-    {
+    static public function postUpdate(CommandEvent $event) {
         self::installBootstrap($event);
     }
 
-    static private function installBootstrap(CommandEvent $event)
-    {
+    static private function installBootstrap(CommandEvent $event) {
         $event->getIO()->write('<info>Generating bootstrap assets</info>');
 
         $options = self::getOptions($event);
@@ -35,16 +32,11 @@ class ScriptHandler
         self::createDirectory("$webDir/js");
         self::createDirectory("$webDir/img");
 
-        /*require 'vendor/leafo/lessphp/lessc.inc.php';
-        $lessc = new \lessc();
-        $css = $lessc->compileFile("$bootstrapDir/less/bootstrap.less");
-        file_put_contents("$webDir/css/bootstrap.css", $css);*/
-
         foreach (glob("$bootstrapDir/less/*.less") as $src) {
             $dst = "$webDir/css/" . basename($src);
             copy($src, $dst);
-        }        
-        
+        }
+
         foreach (glob("$bootstrapDir/js/*.js") as $src) {
             $dst = "$webDir/js/" . basename($src);
             copy($src, $dst);
@@ -56,19 +48,18 @@ class ScriptHandler
         }
     }
 
-    static private function createDirectory($name)
-    {
+    static private function createDirectory($name) {
         if (!is_dir($name)) {
             mkdir($name);
         }
     }
 
-    static protected function getOptions(CommandEvent $event)
-    {
+    static protected function getOptions(CommandEvent $event) {
         $options = array_merge(array(
             'nette-web-dir' => 'www',
-        ), $event->getComposer()->getPackage()->getExtra());
+                ), $event->getComposer()->getPackage()->getExtra());
 
         return $options;
     }
+
 }
